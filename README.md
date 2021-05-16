a5c23b7df90b37c5647293fbf6780798e2aaf98e
Tejas <tejasftw117@gmail.com>
2021-05-16 20:05:02 +0530

    First commit of final solution

---
+++ README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with))
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+
+
+### Links
+
+- [Solution URL](https://your-solution-url.com)
+- [Live Site URL](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+
+### Continued development
+
+If I get a better a way to change 'themes', it will be surely updated..
+
+
+## Author
+
+- Website - [Tejas](https://www.your-site.com)
+- Frontend Mentor - [@Tejas-117](https://www.frontendmentor.io/profile/yourusername)
---
+++ js/script.js
@@ -0,0 +1,102 @@
+// ---TO CHANGE THE 'THEMES'---
+const button = document.querySelector('#btn');
+const toggle = document.querySelector('.toggle');
+
+const bodyClass = document.body.classList;// to toggle between themes using different classes;
+
+   // few fixes for theme one ('color' issue);
+   const topSection = document.querySelector('.top');
+   const outputScreen = document.querySelector('.screen')
+   function topColor(){
+      topSection.style.color = 'white';
+      outputScreen.style.color = 'white';
+   }
+   topColor();
+
+
+let theme = 1, margLeft = 3;// default values of the two;
+button.addEventListener('click', (e) => { 
+      theme += 1; 
+
+      if(theme!=1){// remove the color for other themes applied above;
+         topSection.style.color = '';
+      }
+
+
+      if(theme>3){// reset all the default values;
+         theme=1;
+         margLeft = 3;
+         bodyClass.remove('theme2', 'theme3');
+         topColor();
+      }
+      else if(theme<=3){
+         margLeft += 17;
+      } 
+      
+      bodyClass.add('theme' + theme);// add themes accrodingly;
+      toggle.style.marginLeft = margLeft + 'px';// add margin to 'toggle' respectively;
+   });
+
+
+// ---FOR CALCULATION---;
+let output = document.querySelector('.output');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const others = document.querySelectorAll('.key1');
+
+let ans = '';
+
+   numbers.forEach((number) => {
+      number.addEventListener('click', () => { 
+         print(number.innerText);
+      });
+   });   
+   
+   operators.forEach((operator) => {
+      operator.addEventListener('click', () => {
+         operation(operator.innerText);
+        });
+   });
+
+   others.forEach((other) => {
+      other.addEventListener('click', () => {
+         otherFunctions(other.innerText);
+      });
+   });
+        
+
+// for 'reset' and 'delete';   
+function otherFunctions(ch){
+   if(ch=='RESET'){
+      output.innerText = ' ';
+      ans = '0';
+   }
+   else if(ch=='DEL'){
+      ans = ans.substr(0, ans.length-1);
+      output.innerText = ans;
+   }
+}
+
+// to perform the operation;
+function operation(op){   
+   let x = eval(ans);   
+         
+   ans = x; // update the evaluated answer;
+
+   if(op == '='){// do not append '=' sign;
+      print('');
+   }   
+   else if(op == 'x'){// 'multiply';
+     ans += '*';
+     output.innerText = ans;
+   }
+   else{
+      print(op);
+   }
+}
+
+// diplays the 'numbers';
+function print(ch){
+   ans += ch;
+   output.innerText = ans;
+}
